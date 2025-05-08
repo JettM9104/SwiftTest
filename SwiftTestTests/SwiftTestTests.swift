085d3f345e5d7fd87271bee84e2b38d67ab2637b
JMu <64600882+JettM9104@users.noreply.github.com>
2025-05-08 10:54:53 -0400

    Initial Commit

---
+++ SwiftTestTests/SwiftTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestTests.swift
+//  SwiftTestTests
+//
+//  Created by Jett Mu on 2025-05-08.
+//
+
+import Testing
+@testable import SwiftTest
+
+struct SwiftTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
